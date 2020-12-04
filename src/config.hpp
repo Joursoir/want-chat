6f43ecd3758f445249db2bd4c4b32b912243cbdd
Joursoir <chat@joursoir.net>
2020-12-04 15:38:27 +0000

    add config file for server

---
+++ src/config.hpp
@@ -0,0 +1,14 @@
+#ifndef WANTCHATCONFIG_H
+#define WANTCHATCONFIG_H
+
+// config for work in localhost:
+const char *SERVER_IP = "127.0.0.1";
+const int SERVER_PORT = 3030;
+
+const char *DATABASE_HOST = "localhost";
+const char *DATABASE_USER = "admin";
+const char *DATABASE_PASSWD = "admin";
+const char *DATABASE_DB = "wantchat";
+const unsigned int DATABASE_PORT = 3306; // standard mariadb port
+
+#endif
